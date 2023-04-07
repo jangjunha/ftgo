eeec83ea7944c787109e3a6e27fb628073fb9d67
jangjunha <hi@jangjunha.me>
2023-04-07 16:29:44 +0900

    misc: start

---
+++ src/test/java/me/jangjunha/ftgo/FtgoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jangjunha.ftgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
