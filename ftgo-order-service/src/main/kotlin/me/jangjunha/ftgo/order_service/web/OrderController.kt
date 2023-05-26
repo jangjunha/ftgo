7c80ed464471784b62fae4cf2b9e06ea1c5243d4
jangjunha <hi@jangjunha.me>
2023-05-26 15:18:42 +0900

    feat: add basic order web endpoints

---
+++ ftgo-order-service/src/main/kotlin/me/jangjunha/ftgo/order_service/web/OrderController.kt
@@ -0,0 +1,31 @@
+package me.jangjunha.ftgo.order_service.web
+
+import me.jangjunha.ftgo.order_service.api.DeliveryInformation
+import me.jangjunha.ftgo.order_service.service.OrderService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import java.time.OffsetDateTime
+
+@RestController
+@RequestMapping(path = ["/orders/"])
+class OrderController @Autowired constructor(
+    private val orderService: OrderService
+) {
+    @RequestMapping(method = [RequestMethod.POST])
+    fun createOrder(@RequestBody body: CreateOrderRequest): CreateOrderResponse {
+        val deliveryInformation = DeliveryInformation(
+            OffsetDateTime.now().plusMinutes(60),
+            body.deliveryAddress,
+        )
+        val order = orderService.createOrder(
+            body.consumerId,
+            body.restaurantId,
+            body.items,
+            deliveryInformation,
+        )
+        return CreateOrderResponse(order.id)
+    }
+}
