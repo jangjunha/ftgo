6655edceb9a65a5913c634936cf3c80f2575e650
jangjunha <hi@jangjunha.me>
2023-07-15 13:12:31 +0900

    fix: kitchen api changes on order saga

--- ftgo-order-service/src/main/kotlin/me/jangjunha/ftgo/order_service/sagas/createorder/CreateOrderSagaState.kt
+++ ftgo-order-service/src/main/kotlin/me/jangjunha/ftgo/order_service/sagas/createorder/CreateOrderSagaState.kt
@@ -39,7 +39,11 @@ data class CreateOrderSagaState (
     fun makeCreateTicketCommand(): CreateTicket {
         logger.info("makeCreateTicketCommand")
         val ticketDetails = TicketDetails(orderDetails.lineItems.map {
-            TicketLineItem(it.quantity, it.menuItemId, it.name)
+            TicketLineItem.newBuilder()
+                .setQuantity(it.quantity)
+                .setMenuItemId(it.menuItemId)
+                .setName(it.name)
+                .build()
         })
         return CreateTicket(orderId, ticketDetails, orderDetails.restaurantId)
     }
