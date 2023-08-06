b3a2b18eefdaf3fe6592f7d4f5dd227d6b1fb27c
jangjunha <hi@jangjunha.me>
2023-08-07 00:47:32 +0900

    feat: impl create order rpc

--- ftgo-order-service/src/main/kotlin/me/jangjunha/ftgo/order_service/grpc/OrderServiceImpl.kt
+++ ftgo-order-service/src/main/kotlin/me/jangjunha/ftgo/order_service/grpc/OrderServiceImpl.kt
@@ -2,11 +2,13 @@ package me.jangjunha.ftgo.order_service.grpc
 
 import io.grpc.Status
 import io.grpc.stub.StreamObserver
+import me.jangjunha.ftgo.order_service.api.CreateOrderPayload
 import me.jangjunha.ftgo.order_service.api.GetOrderPayload
 import me.jangjunha.ftgo.order_service.api.Order
 import me.jangjunha.ftgo.order_service.api.OrderServiceGrpc;
-import me.jangjunha.ftgo.order_service.domain.OrderNotFoundException
+import me.jangjunha.ftgo.order_service.domain.*
 import me.jangjunha.ftgo.order_service.service.OrderService
+import java.time.OffsetDateTime
 import java.util.*
 
 class OrderServiceImpl(
@@ -18,6 +20,28 @@ class OrderServiceImpl(
             orderService.getOrder(UUID.fromString(request.id))
         } catch (e: OrderNotFoundException) {
             responseObserver.onError(Status.NOT_FOUND.withCause(e).asRuntimeException())
+            return
+        }
+        responseObserver.onNext(order.toAPI())
+        responseObserver.onCompleted()
+    }
+
+    override fun createOrder(request: CreateOrderPayload, responseObserver: StreamObserver<Order>) {
+        val order = try {
+            orderService.createOrder(
+                UUID.fromString(request.consumerId),
+                UUID.fromString(request.restaurantId),
+                request.itemsList.map { MenuItemIdAndQuantity(it.menuItemId, it.quantity) },
+                DeliveryInformation(
+                    deliveryTime = OffsetDateTime.now().plusMinutes(60),
+                    deliveryAddress = request.deliveryAddress,
+                ),
+            )
+        } catch (e: RestaurantNotFoundException) {
+            responseObserver.onError(Status.INVALID_ARGUMENT.withCause(e).withDescription("invalid restaurant id").asRuntimeException())
+            return
+        } catch (e: InvalidMenuItemIdException) {
+            responseObserver.onError(Status.INVALID_ARGUMENT.withCause(e).withDescription("invalid menuItemId ${e.menuItemId}").asRuntimeException())
             return
         }
         responseObserver.onNext(order.toAPI())
