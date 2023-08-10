24de52b0184f5ad87b9c906e70f886332cc47677
jangjunha <hi@jangjunha.me>
2023-08-10 23:34:31 +0900

    test: add test for Restaurant aggregate

---
+++ ftgo-restaurant-service/src/test/java/me/jangjunha/ftgo/restaurant_service/domain/RestaurantTest.java
@@ -0,0 +1,30 @@
+package me.jangjunha.ftgo.restaurant_service.domain;
+
+import me.jangjunha.ftgo.common.Money;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+    private static final MenuItem AMERICANO = makeMenuItem("americano", "Americano", new Money("1500"));
+    private static final MenuItem LATTE = makeMenuItem("latte", "Cafe Latte", new Money("2500"));
+    private static final Restaurant RESTAURANT = new Restaurant("Rem Cafe", Arrays.asList(AMERICANO, LATTE));
+
+    @Test
+    void findMenuItem() {
+        assertEquals(Optional.of(AMERICANO), RESTAURANT.findMenuItem("americano"));
+        assertEquals(Optional.of(LATTE), RESTAURANT.findMenuItem("latte"));
+        assertEquals(Optional.empty(), RESTAURANT.findMenuItem("strawberry-latte"));
+    }
+
+    private static MenuItem makeMenuItem(String id, String name, Money price) {
+        MenuItem mi = new MenuItem();
+        mi.setId(id);
+        mi.setName(name);
+        mi.setPrice(price);
+        return mi;
+    }
+}
