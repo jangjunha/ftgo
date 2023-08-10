1798a7699e8961b367a9348ce51d479db767b5e6
jangjunha <hi@jangjunha.me>
2023-08-10 20:26:45 +0900

    refactor: use internal constructor instead of default value for reflection

--- ftgo-order-service-api/src/main/kotlin/me/jangjunha/ftgo/order_service/api/OrderDetails.kt
+++ ftgo-order-service-api/src/main/kotlin/me/jangjunha/ftgo/order_service/api/OrderDetails.kt
@@ -4,11 +4,11 @@ import me.jangjunha.ftgo.common.Money
 import java.util.UUID
 
 data class OrderDetails(
-    var lineItems: List<LineItem> = listOf(),
-    var orderTotal: Money = Money.ZERO,
+    var lineItems: List<LineItem>,
+    var orderTotal: Money,
 
-    var restaurantId: UUID = UUID(0, 0),
-    var consumerId: UUID = UUID(0, 0),
+    var restaurantId: UUID,
+    var consumerId: UUID,
 ) {
     data class LineItem(
         var quantity: Int = 0,
@@ -16,4 +16,6 @@ data class OrderDetails(
         var name: String = "",
         var price: Money = Money.ZERO,
     )
+
+    internal constructor() : this(emptyList(), Money.ZERO, UUID(0, 0), UUID(0, 0))
 }
