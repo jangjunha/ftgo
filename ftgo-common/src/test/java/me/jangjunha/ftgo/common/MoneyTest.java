6a53e5efc6798775d84460729d2462b108900d87
jangjunha <hi@jangjunha.me>
2023-08-10 20:00:32 +0900

    test: add unit test for Money value object

--- ftgo-common/src/test/java/me/jangjunha/ftgo/common/MoneyTest.java
+++ ftgo-common/src/test/java/me/jangjunha/ftgo/common/MoneyTest.java
@@ -5,6 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
+
+    private Money m1 = new Money(10);
+    private Money m2 = new Money("20.5");
+
+    @Test
+    public void shouldAdd() {
+        assertEquals(new Money("30.5"), m1.add(m2));
+    }
+
+    @Test
+    public void shouldMultiply() {
+        assertEquals(new Money("61.5"), m2.multiply(3));
+    }
+
     @Test
     public void shouldCompare() {
         assertTrue(new Money(50).isGreaterThanOrEqual(new Money(50)));
