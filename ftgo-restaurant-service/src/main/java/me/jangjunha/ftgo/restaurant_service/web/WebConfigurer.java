3380f5fc88660e58d0a10fc1c6732795de7c82c8
jangjunha <hi@jangjunha.me>
2023-09-23 14:56:44 +0900

    fix: cannot authenticate to restaurant service

--- ftgo-restaurant-service/src/main/java/me/jangjunha/ftgo/restaurant_service/web/WebConfigurer.java
+++ ftgo-restaurant-service/src/main/java/me/jangjunha/ftgo/restaurant_service/web/WebConfigurer.java
@@ -1,11 +1,13 @@
 package me.jangjunha.ftgo.restaurant_service.web;
 
 import me.jangjunha.ftgo.common.web.AuthContextArgumentResolver;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Configuration
 public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
