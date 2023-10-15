ceb40a6acb531e65e8481c4833af91c0755fc11b
jangjunha <hi@jangjunha.me>
2023-10-15 20:52:08 +0900

    feat: configure delivery service message handlers

--- ftgo-delivery-service/src/main/kotlin/me/jangjunha/ftgo/delivery_service/messaging/DeliveryServiceMessagingConfiguration.kt
+++ ftgo-delivery-service/src/main/kotlin/me/jangjunha/ftgo/delivery_service/messaging/DeliveryServiceMessagingConfiguration.kt
@@ -1,9 +1,12 @@
 package me.jangjunha.ftgo.delivery_service.messaging
 
+import io.eventuate.tram.events.subscriber.DomainEventDispatcherFactory
 import io.eventuate.tram.spring.consumer.common.TramNoopDuplicateMessageDetectorConfiguration
 import io.eventuate.tram.spring.events.common.TramEventsCommonAutoConfiguration
 import io.eventuate.tram.spring.events.subscriber.TramEventSubscriberConfiguration
 import io.eventuate.tram.spring.messaging.common.TramMessagingCommonAutoConfiguration
+import me.jangjunha.ftgo.delivery_service.domain.DeliveryService
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
@@ -15,4 +18,14 @@ import org.springframework.context.annotation.Import
     TramEventsCommonAutoConfiguration::class,
     TramNoopDuplicateMessageDetectorConfiguration::class,
 )
-class DeliveryServiceMessagingConfiguration
+class DeliveryServiceMessagingConfiguration {
+
+    @Bean
+    fun deliveryMessageHandlers(deliveryService: DeliveryService) = DeliveryServiceMessageHandlers(deliveryService)
+
+    @Bean
+    fun domainEventDispatcher(
+        deliveryMessageHandlers: DeliveryServiceMessageHandlers,
+        domainEventDispatcherFactory: DomainEventDispatcherFactory,
+    ) = domainEventDispatcherFactory.make("deliveryServiceEvents", deliveryMessageHandlers.domainEventHandlers())
+}
