eb31a0733275125f28a1adc2b4ed3a3f8db0b438
jangjunha <hi@jangjunha.me>
2023-10-15 20:52:40 +0900

    feat: create delivery when order created

--- ftgo-delivery-service/src/main/kotlin/me/jangjunha/ftgo/delivery_service/domain/DeliveryService.kt
+++ ftgo-delivery-service/src/main/kotlin/me/jangjunha/ftgo/delivery_service/domain/DeliveryService.kt
@@ -1,6 +1,7 @@
 package me.jangjunha.ftgo.delivery_service.domain
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -9,9 +10,23 @@ import java.util.UUID
 class DeliveryService
 @Autowired constructor(
     private val restaurantRepository: RestaurantRepository,
+    private val deliveryRepository: DeliveryRepository,
 ) {
 
     fun upsertRestaurant(id: UUID, name: String, address: String) {
         restaurantRepository.save(Restaurant(id, name, address))
+    }
+
+    fun createDelivery(orderId: UUID, restaurantId: UUID, address: String) {
+        val restaurant =
+            restaurantRepository.findByIdOrNull(restaurantId) ?: throw RestaurantNotFoundException(restaurantId)
+        deliveryRepository.save(
+            Delivery(
+                id = orderId,
+                restaurantId = restaurantId,
+                pickupAddress = restaurant.address,
+                deliveryAddress = address,
+            )
+        )
     }
 }
--- ftgo-delivery-service/src/main/kotlin/me/jangjunha/ftgo/delivery_service/messaging/DeliveryServiceMessageHandlers.kt
+++ ftgo-delivery-service/src/main/kotlin/me/jangjunha/ftgo/delivery_service/messaging/DeliveryServiceMessageHandlers.kt
@@ -3,6 +3,8 @@ package me.jangjunha.ftgo.delivery_service.messaging
 import io.eventuate.tram.events.subscriber.DomainEventEnvelope
 import io.eventuate.tram.events.subscriber.DomainEventHandlersBuilder
 import me.jangjunha.ftgo.delivery_service.domain.DeliveryService
+import me.jangjunha.ftgo.order_service.api.OrderServiceChannels
+import me.jangjunha.ftgo.order_service.api.events.OrderCreated
 import me.jangjunha.ftgo.restaurant_service.api.events.RestaurantCreated
 import org.springframework.beans.factory.annotation.Autowired
 import java.util.*
@@ -15,10 +17,17 @@ class DeliveryServiceMessageHandlers
     fun domainEventHandlers() = DomainEventHandlersBuilder
         .forAggregateType("me.jangjunha.ftgo.restaurant_service.domain.Restaurant")
         .onEvent(RestaurantCreated::class.java, this::handleRestaurantCreated)
+        .andForAggregateType(OrderServiceChannels.ORDER_EVENT_CHANNEL)
+        .onEvent(OrderCreated::class.java, this::handleOrderCreated)
         .build()
 
     private fun handleRestaurantCreated(de: DomainEventEnvelope<RestaurantCreated>) {
         val restaurantId = UUID.fromString(de.aggregateId)
         deliveryService.upsertRestaurant(restaurantId, de.event.name, de.event.address)
+    }
+
+    private fun handleOrderCreated(de: DomainEventEnvelope<OrderCreated>) {
+        val orderId = UUID.fromString(de.aggregateId)
+        deliveryService.createDelivery(orderId, de.event.orderDetails.restaurantId, de.event.deliveryAddress)
     }
 }
